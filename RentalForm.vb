577841ae5c9a24dbf336f6ee15a6fe5c6bbefd66
Justin Bell <Juxin203@gmail.com>
2024-12-13 16:12:54 -0700

    input validation complete

--- RentalForm.vb
+++ RentalForm.vb
@@ -30,25 +30,78 @@ Public Class RentalForm
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
         Dim incorrect As String = ""
         Dim zip As Integer
-        If NameTextBox.Text IsNot Text Then
+        Dim startOD As Integer
+        Dim endOD As Integer
+        Dim days As Integer
+
+        If String.IsNullOrWhiteSpace(NameTextBox.Text) Then
             incorrect &= "Enter a valid name." & vbCrLf
         End If
 
-        If CBool(AddressTextBox.Text) = False Then
+        If String.IsNullOrWhiteSpace(AddressTextBox.Text) Then
             incorrect &= "Enter an address." & vbCrLf
         End If
 
-        If StateTextBox.Text IsNot Text Then
+        If String.IsNullOrWhiteSpace(StateTextBox.Text) Then
             incorrect &= "Enter a valid state." & vbCrLf
         End If
 
         If Integer.TryParse(ZipCodeTextBox.Text, zip) Then
-            If zip > 99999 Then
+            If zip > 99999 Or zip < 0 Then
                 incorrect &= "Enter a 5-digit zip code." & vbCrLf
+            End If
+        Else
+            If String.IsNullOrWhiteSpace(ZipCodeTextBox.Text) Then
+                incorrect &= "Enter a zip code." & vbCrLf
+            Else
+                incorrect &= "Enter a numeric zip code." & vbCrLf
             End If
         End If
 
+        If Integer.TryParse(BeginOdometerTextBox.Text, startOD) Then
+            If startOD < 0 Then
+                incorrect &= "Enter a valid odometer reading." & vbCrLf
+            End If
+        Else
+            If String.IsNullOrWhiteSpace(BeginOdometerTextBox.Text) Then
+                incorrect &= "Enter valid odometer reading." & vbCrLf
+            Else
+                incorrect &= "Enter a valid odometer reading." & vbCrLf
+            End If
+        End If
 
+        If Integer.TryParse(EndOdometerTextBox.Text, endOD) Then
+            If startOD < 0 Then
+                incorrect &= "Enter a valid odometer reading." & vbCrLf
+            End If
+        Else
+            If String.IsNullOrWhiteSpace(BeginOdometerTextBox.Text) Then
+                incorrect &= "Enter valid odometer reading." & vbCrLf
+            Else
+                incorrect &= "Enter a valid odometer reading." & vbCrLf
+            End If
+        End If
+
+        If startOD > endOD Then
+            incorrect &= "End odometer reading must be larger than beginning odometer reading." & vbCrLf
+        End If
+
+        If Integer.TryParse(DaysTextBox.Text, days) Then
+            If days > 45 Or days < 0 Then
+                incorrect &= "Rental duration exceeds maxium allowed time" & vbCrLf
+            End If
+        Else
+            If String.IsNullOrWhiteSpace(DaysTextBox.Text) Then
+                incorrect &= "Enter a rental duration." & vbCrLf
+            Else
+                incorrect &= "Enter a number of days to rent." & vbCrLf
+            End If
+        End If
+
+        If incorrect <> "" Then
+            MessageBox.Show(incorrect, "Missing Fields", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
 
     End Sub
+
 End Class
